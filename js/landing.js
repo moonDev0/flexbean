2ab46134e0bafdddbb0538991604ff9b908e690c
MUNIR ABUBAKAR SADIQ <82941983+moonDev0@users.noreply.github.com>
2022-03-17 14:17:51 +0100

    Add files via upload

---
+++ js/landing.js
@@ -0,0 +1,12 @@
+function game(){
+    window.location.href='game.html';
+}
+function home(){
+    window.location.href='index.html';
+}
+function playground(){
+    window.location.href='../playground/index.html';
+}
+function w3school(){
+    window.location.href='https://www.w3schools.com/css/default.asp';
+}
---
+++ js/messages.js
@@ -0,0 +1,57 @@
+var messages = {
+  languageActive: {
+    'en': 'English',
+   
+  },
+  next: {
+    'en': 'Next',
+  },
+  labelFooter: {
+    'en': 'Flexbox bean is created by',
+    },
+  labelLevel: {
+    'en': 'Level',
+    },
+  labelOf: {
+    'en': 'of',
+    },
+  title: {
+    'en': 'Flexbox bean - A game for learning CSS flexbox',
+    },
+  labelReset: {
+    'en': 'Reset',
+    },
+  warningReset: {
+    'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
+    },
+  gridGarden: {
+    'en': 'Want to learn CSS grid? Play',
+    },
+  labelLanguage: {
+    'en': 'Language',
+    },
+  labelSettings: {
+    'en': 'Settings',
+    },
+  labelDifficulty: {
+    'en': 'Difficulty',
+    },
+  labelColorblind: {
+    'en': 'Colorblind Mode',
+    },
+  labelColorblindOff: {
+    'en': 'Off',
+    },
+  labelColorblindOn: {
+    'en': 'On',
+    },
+  labelDifficultyEasy: {
+    'en': 'Beginner',
+    },
+  labelDifficultyMedium: {
+    'en': 'Intermediate - No Directions',
+    },
+  labelDifficultyHard: {
+    'en': 'Expert - No Directions & Random Levels',
+    }
+};
